2d24b0b4792ece43df103d059da734141608d24e
Nusrat Ismoilov <nusrat.bbismoilov@gmail.com>
2023-01-02 23:45:24 +0900

    Update

---
+++ main/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import TodoItem
+
+class TodoItemForm(forms.ModelForm):
+    class Meta:
+        model = TodoItem
+        fields = ('title')
--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200)
+    # description = models.TextField()
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # completed = models.BooleanField(default=False)
